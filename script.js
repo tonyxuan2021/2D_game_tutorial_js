f622cc0edca682cf6da89094236da19ad52d3bea
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-28 20:04:14 -0700

    create new game object from constructors

--- script.js
+++ script.js
@@ -11,7 +11,22 @@ window.addEventListener("load", function () {
 
   class Particle {}
 
-  class Player {}
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+    update() {
+      this.y += this.speedY;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
   class Enemy {}
 
@@ -21,7 +36,23 @@ window.addEventListener("load", function () {
 
   class UI {}
 
-  class Game {}
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {
+      this.player.update();
+    }
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
 });
 
 // encapsulation: access to the data can be restricted from outside the bundle
+
+// constructor is blueprint, using the new keyward to create one new blank Js object
