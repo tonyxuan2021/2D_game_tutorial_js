5446122a6b4dedf599da8500ed2e96dd98fda986
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-28 19:49:02 -0700

    initialize files

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", function () {
+  // canvas setup
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 500;
+
+  class InputHandler {}
+
+  class Projectile {}
+
+  class Particle {}
+
+  class Player {}
+
+  class Enemy {}
+
+  class Layer {}
+
+  class Background {}
+
+  class UI {}
+
+  class Game {}
+});
+
+// encapsulation: access to the data can be restricted from outside the bundle
